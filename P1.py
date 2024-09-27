66571021afc37e92917896b39c9c432891d414fd
DonDexCam <dbryant34@student.ccc.edu>
2024-09-27 16:46:02 -0500

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,16 @@
+width=float(input('Enter width'))
+height=float(input('Enter height'))
+Area=width*height
+print('area is', Area)
+
+
+base=float(input('Enter base'))
+height=float(input('Enter height'))
+Area=base*height*.5
+print('area is', Area)
+
+
+
+radius=float(input('Enter radius'))
+Area=radius**2*3.14159
+print('area is', Area)
---
+++ P3.py
@@ -0,0 +1,24 @@
+pounds=float(input('enter pounds'))
+price_per_pound=.99
+gross_sales=pounds*price_per_pound
+print('gross is', gross_sales)
+discount=0
+if (pounds>=10) and (pounds<99.99):
+    discount=.1
+    print('discount is', discount)
+else:
+    if(pounds>=100) and (pounds<999.99):
+        discount=.2
+        print('discount is', discount)
+    else:
+        if (pounds>=1000) and (pounds<9999.99):
+            discount=.3
+            print('discount is', discount)
+        else:
+            if (pounds>10000):
+                discount=.4
+                print('discount is', discount)
+discount_amt=gross_sales*discount
+final_amt=gross_sales-discount_amt
+print('discount_amt is', discount_amt)
+print('final_amt is', final_amt)
