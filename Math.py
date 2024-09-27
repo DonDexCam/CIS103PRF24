072b61d16179bde0d2384ed157aa37fced6e2d73
DonDexCam <dbryant34@student.ccc.edu>
2024-09-27 16:55:36 -0500

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,24 @@
+10+5-8/2
+15+3/4*10**2**2
+
+width=float(input('enter width'))
+height=float(input('enter height'))
+area=width*height
+print('area is' , area)
+
+base=float(input('Enter base'))
+height=float(input('Enter height'))
+Area=base*height*.5
+print('area is',Area)
+
+
+radius=float(input("Enter radius"))
+Area=radius**2*3.14159
+print("area is", Area)
+
+
+10>20
+10<20
+
+2.5<2.0
+
---
+++ PWhile.py
@@ -0,0 +1,87 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: /Users/donatellobryant/Documents/PWhile.py =============
+     calorie calculation
+-----
+enter running time in minutes5
+minutes: 5 burns: 24.5 calories
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes
+input cannot be blank or less than 5
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes2
+input cannot be blank or less than 5
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes45
+minutes: 5 burns: 24.5 calories
+minutes: 10 burns: 49.0 calories
+minutes: 15 burns: 73.5 calories
+minutes: 20 burns: 98.0 calories
+minutes: 25 burns: 122.50000000000001 calories
+minutes: 30 burns: 147.0 calories
+minutes: 35 burns: 171.5 calories
+minutes: 40 burns: 196.0 calories
+minutes: 45 burns: 220.50000000000003 calories
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes20
+minutes: 5 burns: 24.5 calories
+minutes: 10 burns: 49.0 calories
+minutes: 15 burns: 73.5 calories
+minutes: 20 burns: 98.0 calories
+
+Again y/n?->n
+Traceback (most recent call last):
+  File "/Users/donatellobryant/Documents/PWhile.py", line 3, in <module>
+    while((ans=='y') or (answ=='y')):
+NameError: name 'answ' is not defined. Did you mean: 'ans'?
+>>> 
+============== RESTART: /Users/donatellobryant/Documents/PWhile.py =============
+     calorie calculation
+-----
+enter running time in minutes20
+minutes: 5 burns: 24.5 calories
+minutes: 10 burns: 49.0 calories
+minutes: 15 burns: 73.5 calories
+minutes: 20 burns: 98.0 calories
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes45
+minutes: 5 burns: 24.5 calories
+minutes: 10 burns: 49.0 calories
+minutes: 15 burns: 73.5 calories
+minutes: 20 burns: 98.0 calories
+minutes: 25 burns: 122.50000000000001 calories
+minutes: 30 burns: 147.0 calories
+minutes: 35 burns: 171.5 calories
+minutes: 40 burns: 196.0 calories
+minutes: 45 burns: 220.50000000000003 calories
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes2
+input cannot be blank or less than 5
+
+Again y/n?->y
+     calorie calculation
+-----
+enter running time in minutes
+input cannot be blank or less than 5
+
+Again y/n?->n
+done
