c0599216f16a9346e342d9e8e703c39a0f30e9cd
DonDexCam <dbryant34@student.ccc.edu>
2024-10-17 19:52:44 -0500

    Add files via upload

---
+++ P12.py
@@ -0,0 +1,15 @@
+print('Rainfall for Chicago (2017):')
+numb=[0]*12
+for x in range (0,12):
+    print(x)
+    numb[x]=float(input('Enter rainfall:'))
+    
+len01=len(numb)
+print(numb)
+print('Highest rainfall->',max(numb))
+print('Lowest rainfall->',min(numb))
+print('Total rainfall->',sum(numb))
+a=sum(numb)/12
+print('Average is',a)
+
+
